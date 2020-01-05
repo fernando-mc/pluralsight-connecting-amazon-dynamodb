d4439e4608b3d99f6f47acbf4fa41fd565e41515
Fernando Medina Corey <fmcorey@gmail.com>
2020-01-05 00:11:05 -0800

    First shot at globomantics survey service

---
+++ 03-integrating_dynamodb_in_a_web_service/globomantics-survey-service/backend/create_survey.py
@@ -0,0 +1,26 @@
+import boto3
+import os
+import json
+import uuid
+
+dynamodb = boto3.resource('dynamodb')
+
+TABLE_NAME = os.environ['DYNAMODB_TABLE']
+table = dynamodb.Table(TABLE_NAME)
+
+
+def handler(event, context):
+    # Load and validate submission data
+    survey_data = json.loads(event['body'])
+    
+    survey_id = str(uuid.uuid4())
+    survey_data['survey_id'] = survey_id
+    result = table.put_item(
+        Item=survey_data
+    )
+    response = {
+        "statusCode": 200,
+        "headers": {"Access-Control-Allow-Origin":"*"},
+        "body": json.dumps({"survey_id" : survey_id)
+    }
+    return response
